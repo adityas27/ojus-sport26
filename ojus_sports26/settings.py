28efccd5ef032a54b9be26ff059fa6327a0231e3
Aditya Singh <adityasingh27072746@gmail.com>
2025-12-22 18:28:01 +0530

    MySQL Connection config for server

--- ojus_sports26/settings.py
+++ ojus_sports26/settings.py
@@ -84,6 +84,16 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ojus',
+        'USER': 'ojus_user',
+        'PASSWORD': 'Apsit2k25!',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
 
 
 # Password validation
