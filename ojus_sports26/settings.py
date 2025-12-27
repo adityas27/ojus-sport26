e99dc75fc19338cf8a5735997f24f80cd71df824
Aditya Singh <adityasingh27072746@gmail.com>
2025-12-27 12:06:12 +0530

    Test deploy for culturals

--- ojus_sports26/settings.py
+++ ojus_sports26/settings.py
@@ -83,22 +83,22 @@ WSGI_APPLICATION = 'ojus_sports26.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ojus',
-        'USER': 'ojus_user',
-        'PASSWORD': 'Apsit2k25!',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'ojus',
+#         'USER': 'ojus_user',
+#         'PASSWORD': 'Apsit2k25!',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
 
 
 
