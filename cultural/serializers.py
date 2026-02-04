23b845abece4e64eafd0006545fdf93459ea71b2
Aditya Singh <adityasingh27072746@gmail.com>
2026-02-04 09:58:16 +0530

    minor update

--- cultural/serializers.py
+++ cultural/serializers.py
@@ -3,16 +3,16 @@ from .models import Registration, Event
 
 class RegistrationSerializer(serializers.ModelSerializer):
     # Accept event_slug from client and derive event & student server-side
-    # Also expose event_slug read-only for client-side checks
+    # Also expose event.slug in responses as `event_slug_display`
     event_slug = serializers.CharField(write_only=True, required=True)
-    event_slug = serializers.CharField(source='event.slug', read_only=True)
+    event_slug_display = serializers.CharField(source='event.slug', read_only=True)
     student = serializers.PrimaryKeyRelatedField(read_only=True)
     event = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
         model = Registration
-        fields = ['id', 'student', 'event', 'year', 'registered_on', 'event_slug']
-        read_only_fields = ('id', 'student', 'event', 'registered_on', 'event_slug')
+        fields = ['id', 'student', 'event', 'year', 'registered_on', 'event_slug', 'event_slug_display']
+        read_only_fields = ('id', 'student', 'event', 'registered_on', 'event_slug_display')
     
     def create(self, validated_data):
         event_slug = validated_data.pop('event_slug')
